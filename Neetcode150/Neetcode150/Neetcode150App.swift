67c96057a26aa1076411be47283c0a6986771b8b
Trupti15 <Ahladini02@gmail.com>
2024-12-04 09:49:57 +0530

    Initial setup

---
+++ Neetcode150/Neetcode150/Neetcode150App.swift
@@ -0,0 +1,17 @@
+//
+//  Neetcode150App.swift
+//  Neetcode150
+//
+//  Created by Abhishek-Trupti on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Neetcode150App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
